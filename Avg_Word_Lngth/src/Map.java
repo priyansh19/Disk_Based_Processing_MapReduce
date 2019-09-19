952ac2ce8874f089fc54779aedfbb1905ce7df42
Priyansh Gupta <priyansh7533053411@gmail.com>
2019-09-19 23:38:34 +0530

    Update Map.java

--- Avg_Word_Lngth/src/Map.java
+++ Avg_Word_Lngth/src/Map.java
@@ -4,17 +4,6 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
-
-/**
- *
- * @author training
- *TextInputFOrmat is used which will provide key as an offset and line as value
- *From the mapper o/p key is the word and value will be the file name
- */
-
-
-/*   Mapper Class for Average Word Length problem */
-
 public class Map extends Mapper<LongWritable,Text,Text,IntWritable>{
     String line = new String();
     Text firstLetter = new Text();
